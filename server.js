868f32230b366edcba43282c428f648744b0e2cb
PremierDesign <premier.design.company@gmail.com>
2023-12-13 20:04:17 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const OpenAI = require("openai");
+const cors = require("cors");
+
+app.use(cors());
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// const openai = new OpenAIApi(configuration);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 8020;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.post("/analyze-spending", async (req, res) => {
+  const spendingData = req.body;
+  const prompt = createPrompt(spendingData);
+
+  try {
+    const gptResponse = await openai.completions.create({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 150,
+    });
+
+    res.json({ analysis: gptResponse.choices[0].text });
+  } catch (error) {
+    console.error("Error calling OpenAI API:", error);
+    res.status(500).json({ error: "Error processing request" });
+  }
+});
+
+function createPrompt(data) {
+  let prompt = "Analyze the following spending data for patterns:\n";
+  for (const item of data) {
+    prompt += `Category: ${item.category}, Amount: ${item.amount}\n`;
+  }
+
+  prompt += "\nIdentify any notable spending patterns or insights.";
+
+  return prompt;
+}
